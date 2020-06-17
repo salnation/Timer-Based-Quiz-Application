e1d1cdb2f611866a1bbfcff232187e0f4fe5041c
Stefan Salituro <stefansalituro@gmail.com>
2020-06-17 17:31:21 -0400

    This time I added the variable score to the end.js file. Based on the username and the users score the high scores will be sorted. Then they will be stored in local storage.

--- end.js
+++ end.js
@@ -11,3 +11,16 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     e.preventDefault();
 };
+    // Aiming to attain the users name and their most recent score
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    // Attempting to store high scores to local storage
+    localStorage.setItem('highScores');
+    window.location.assign('/');
+};
