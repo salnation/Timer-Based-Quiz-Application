5785f254b57861a442fa159371f2b1fd039ee291
Stefan Salituro <stefansalituro@gmail.com>
2020-06-16 21:25:21 -0400

    Seventh commit: The questions.js file has been updated to include two constants and 5 variables - current question, accepting answers, score, question counter, and available questions.

--- questions.js
+++ questions.js
@@ -1,13 +1,18 @@
-var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    ///etc.
-  ];
+// Need to reference html elements
+
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+//Create some variables
+
+// This is going to be an object
+let currentQuestion = {};
+// Delay before user can answer again 
+let acceptingAnswers = false;
+// starts at zero
+let score = 0;
+// starts at zero
+let questionCounter = 0;
+// A copy of the full question set and so that there will always be a unique question from the ones before.
+let availableQuesions = [];
+
